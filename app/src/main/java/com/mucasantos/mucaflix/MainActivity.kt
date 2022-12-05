531e7424ae1f48c1b47190148cf0d7c32f11b06c
Samuel Santos <Samuel@ESN106D10580899.sp.local>
2022-12-05 20:19:37 -0300

    Initial commit

---
+++ app/src/main/java/com/mucasantos/mucaflix/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mucasantos.mucaflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+//Images
+//https://www.pngwing.com/en/free-png-avppf
